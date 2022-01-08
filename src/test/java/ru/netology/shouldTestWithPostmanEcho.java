12c18cc02dca139b35ea0b83025008b30604b946
Ufimskii <91329112+Ufimskii@users.noreply.github.com>
2022-01-08 21:46:33 +0500

    AT_1_2_Post_down

---
+++ src/test/java/ru/netology/shouldTestWithPostmanEcho.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class shouldTestWithPostmanEcho {
+    @Test
+    void shouldTestPostman() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data<For><Crash>"))
+        ;
+    }
+}
